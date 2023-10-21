0dc490e3b1fbd0612e3bbb613ede5419fdd0126c
justice-udehegbunam <justicefavour491@gmail.com>
2023-10-21 12:06:18 +0100

    My tailwind personal portfolio website completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
         'sans': ['Poppins', 'sans'],
         'serif': ['Montserrat', 'serif'],
       },
+      screens: {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'}, 
+        'tallscreen': {'raw': '(min-aspect-ratio: 13/20)'}, 
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
+      margin: {
+        '20': '5rem',
+      },
     },
   },
   plugins: [],
